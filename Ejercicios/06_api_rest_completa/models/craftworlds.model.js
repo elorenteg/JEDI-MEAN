2522e8d9c0a96028da857eaf7bc90555475f98da
Ester Lorente <ester.lorente@est.fib.upc.edu>
2016-07-04 01:13:38 +0200

    Ejercicio 6 - API REST completa (Extra 2 - bucle de findOnes)

--- Ejercicios/06_api_rest_completa/models/craftworlds.model.js
+++ Ejercicios/06_api_rest_completa/models/craftworlds.model.js
@@ -5,7 +5,9 @@ module.exports = function() {
     
     var craftworldSchema = new Schema({
         nombre: { type: String, unique: true },
-        poblacion: { type: Number, default: 0, min: 0 }
+        poblacion: { type: Number, default: 0, min: 0 },
+        coordenada_x: { type: Number, required: true, min : 0, max: 90 },
+        coordenada_y: { type: Number, required: true, min : 0, max: 90 }
     });
     
     mongoose.model('Craftworld', craftworldSchema, 'craftworlds');
--- Ejercicios/06_api_rest_completa/routes/usuario.js
+++ Ejercicios/06_api_rest_completa/routes/usuario.js
@@ -17,6 +17,8 @@ var Craftworld = require('mongoose').model('Craftworld');
 
 var makerRecursos = require('../helpers/makerRecurso');
 
+var MAX_DISTANCE = 100; // 500 km
+
 router.get('/recursos', express_jwt({secret: jwt_secret, requestProperty: 'usuario', credentialsRequired: false}), function(req, res) {
     if (req.usuario) {
         var rango = req.usuario.rango;
@@ -44,19 +46,57 @@ function jsonMessage(mss, json, res, nstatus) {
 }
 
 function getRecursos(specificRecurso, rango, req, res) {
-    specificRecurso.find({craftworld:req.params.nombre_craft}, function(error, result) {
-        if (!error) getPuertas(result,rango,req,res);
+    specificRecurso.find({}, function(error, result) {
+        if (!error) {
+            getPuertas(result,rango,req,res);
+        }
         else sendMessage("Error al obtener los recursos", res, STATUS_ERROR);
     });
 }
 
 function getPuertas(recursos, rango, req, res) {
-    Puerta.find({craftworld:req.params.nombre_craft, rangos:rango}, function(error, result) {
+    Puerta.find({rangos:rango}, function(error, result) {
         if (!error) {
             recursos = recursos.concat(result);
-            if (recursos.length === 0) sendMessage("No hay ningun Recurso accesible por el Usuario de rango " +  rango, res, STATUS_ERROR);
-            else jsonMessage("Info de los recursos accesible por el Usuario de rango " + rango, recursos, res, STATUS_OK);
+            Craftworld.findOne({nombre: req.params.nombre_craft}, function(error, craft_user) {
+                filterByDistanceI([], recursos, 0, req.params.nombre_craft, craft_user, res);
+            });
         }
         else sendMessage("Error al obtener los recursos", res, STATUS_ERROR);
     });
+}
+
+function filterByDistanceI(resultat, recursos, i, nombre_craft_user, craft_user, res) {
+    if (i == recursos.length) {
+        if (resultat.length === 0) sendMessage("No hay ningun Recurso accesible por el Usuario", res, STATUS_ERROR);
+        else jsonMessage("Info de los recursos accesible por el Usuario", resultat, res, STATUS_OK);
+    }
+    else {
+        Craftworld.findOne({nombre: recursos[i].craftworld}, function(error, craft_recurs) {
+            var dist_crafts = distance(craft_recurs, craft_user);
+            if (craft_recurs.craftworld === nombre_craft_user || dist_crafts < MAX_DISTANCE) {
+                resultat[resultat.length] = recursos[i];
+            }
+            filterByDistanceI(resultat, recursos, i+1, nombre_craft_user, craft_user, res);
+        });
+    }
+}
+
+function distance(craft_recurs, craft_user) {
+    var lat1 = craft_recurs.coordenada_y, lon1 = craft_recurs.coordenada_x;
+    var lat2 = craft_user.coordenada_y, lon2 = craft_user.coordenada_x;
+    
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+    var dLon = deg2rad(lon2 - lon1); 
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
 }
